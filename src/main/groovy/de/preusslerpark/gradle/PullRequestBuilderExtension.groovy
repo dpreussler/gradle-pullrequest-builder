3a07d795756d6366bddddbee0eaff663c9aa91fc
fixed <preusd@CL02028.local>
2017-01-19 15:56:14 +0100

    initial commit

---
+++ src/main/groovy/de/preusslerpark/gradle/PullRequestBuilderExtension.groovy
@@ -0,0 +1,18 @@
+package de.preusslerpark.gradle
+
+import org.gradle.api.Project
+
+class PullRequestBuilderExtension {
+
+    String githubUri
+    String user
+    String accessToken
+    String branchSuffix
+    String master
+    String source = 'src'
+    String title = 'automated pull request'
+    String message = 'this is a generated pull request'
+
+    public PullRequestBuilderExtension(Project project) {
+    }
+}
---
+++ src/main/groovy/de/preusslerpark/gradle/PullRequestBuilderPlugin.groovy
@@ -0,0 +1,67 @@
+package de.preusslerpark.gradle
+
+import com.github.kittinunf.fuel.Fuel
+import groovy.transform.PackageScope
+import kotlin.Pair
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+
+import java.nio.charset.Charset
+
+class PullRequestBuilderPlugin implements Plugin<Project> {
+
+    @PackageScope static final String GIT_TASK = 'gitCommit'
+    @PackageScope static final String PR_TASK = 'gitPullRequest'
+    def String branchName
+
+    @Override
+    void apply(Project project) {
+        PullRequestBuilderExtension extension = project.extensions.create('gitPullRequest', PullRequestBuilderExtension, project)
+
+        Task git = createGitTask(project, extension)
+        Task gitHub = createPullRequestTask(project, extension)
+        gitHub.dependsOn git
+    }
+
+    private Task createGitTask(Project project, PullRequestBuilderExtension extension) {
+        Task task = project.tasks.create(GIT_TASK)
+        task.outputs.upToDateWhen { false }
+        task.with {
+            group = 'publishing'
+            description = 'Commits and pushes changes to git.'
+            doFirst {
+                branchName = extension.branchSuffix + new Date().getTime().toString()
+            }
+            doLast {
+                "git branch ${branchName}".execute().waitFor()
+                "git checkout ${branchName}".execute().waitFor()
+                "git add ${extension.source}".execute().waitFor()
+                "git commit -m ${extension.message}".execute().waitFor()
+                "git push".execute().waitFor()
+            }
+        }
+        return task
+    }
+
+    private Task createPullRequestTask(Project project, PullRequestBuilderExtension extension) {
+        Task task = project.tasks.create(PR_TASK)
+        task.outputs.upToDateWhen { false }
+        task.with {
+            group = 'publishing'
+            description = 'Creates Pull request on github.'
+            onlyIf { dependsOnTaskDidWork() }
+            doLast {
+                println Fuel.post("${extension.githubUri.replaceAll("github.com", "api.github.com/repos") + "/pulls" }")
+                .authenticate("${extension.user}", "${extension.accessToken}")
+                        .body("{ \"title\" : \"${extension.title}\", " +
+                        "\"body\" : \"${extension.message}\"," +
+                        "\"head\" : \"${branchName}\"," +
+                        "\"base\" : \"${extension.master}\" }", Charset.forName("utf-8"))
+                .header(new Pair<String, Object>("Content-Type", "application/json"))
+                .response().toString()
+            }
+        }
+        return task
+    }
+}
